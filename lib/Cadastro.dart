51f9826bc6dcd11c13efd5d5ce80793989422950
João Victor Nery <joaovictornery49@gmail.com>
2026-08-15 18:11:07 -0300

    Parametros adicionado na tela de cadastro

--- lib/Cadastro.dart
+++ lib/Cadastro.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:horas/Reutilizaveis/Textfield.dart';
 
 class Cadastro extends StatefulWidget {
   State<Cadastro> createState() => CadastroState();
 }
 
 class CadastroState extends State<Cadastro> {
+  TextEditingController senha = TextEditingController();
+  TextEditingController csenha = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController nome = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +38,38 @@ class CadastroState extends State<Cadastro> {
                 Text(
                   'Cadastro',
                   style: TextStyle(fontSize: 30, color: Colors.black),
+                ),
+
+                SizedBox(height: 40),
+                Texto(
+                  esenha: false,
+                  controller: nome,
+                  labelText: 'Nome',
+                  hintText: 'Digite seu nome',
+                ),
+                SizedBox(height: 20),
+
+                Texto(
+                  esenha: false,
+                  controller: email,
+                  labelText: 'Email',
+                  hintText: 'Digite seu email',
+                ),
+
+                SizedBox(height: 20),
+
+                Texto(
+                  esenha: false,
+                  controller: senha,
+                  labelText: 'Senha',
+                  hintText: 'Digite sua senha',
+                ),
+                SizedBox(height: 20),
+                Texto(
+                  esenha: true,
+                  controller: csenha,
+                  labelText: 'Confirme sua senha',
+                  hintText: 'Digite a sua senha novamente',
                 ),
               ],
             ),
