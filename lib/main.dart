5e672200dcb342c3ead4948a476ce2dfaaf4c860
João Victor Nery <joaovictornery49@gmail.com>
2026-08-19 19:44:50 -0300

    erro resolvido

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:horas/Login.dart';
+import 'package:horas/Telas/Login.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
