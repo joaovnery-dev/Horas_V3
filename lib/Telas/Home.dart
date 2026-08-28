d2991497346916e125d6012856859a68cdf439fe
joaovnery <joaovictornery49@gmail.com>
2026-08-27 22:08:37 -0300

    lista feita

--- lib/Telas/Home.dart
+++ lib/Telas/Home.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatefulWidget {
   final User user;
 
-  Home({super.key, required this.user});
+  const Home({super.key, required this.user});
   
+  @override
   State<Home> createState() => HomeState();
 
   
 }
 
 class HomeState extends State<Home> {
-  
+  List<Hour> listahoras =[];
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void initState() {
+    super.initState();
+    
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Menu(user: widget.user),
+      appBar: AppBar(
+        title: Text('NeryHour'),
+      ),
       backgroundColor: Colors.black,
-      body: Center(child: Text('Home')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Ação ao pressionar o botão
+        },
+        child: Icon(Icons.add),
+      ),
+      body: (listahoras.isEmpty) ? 
+      Center(child: Text('Nenhuma hora registrada \n Vamos registrar uma?',textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 18),)) :
+
     );
   }
 }
